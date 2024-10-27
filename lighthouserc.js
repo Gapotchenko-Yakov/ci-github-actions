1d72de20d3d601a3768c70a834a746dde850c006
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-10-27 22:05:32 +0300

    extra lighthouse ci restrictions

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,11 @@ module.exports = {
         assert: {
             preset: 'lighthouse:recommended',
             assertions: {
-                "errors-in-console": "off",
+                'errors-in-console': 'warn',
+                'categories:performance': ['error', { minScore: 0.95 }],
+                'categories:accessibility': ['error', { minScore: 0.95 }],
+                'categories:best-practices': ['error', { minScore: 0.95 }],
+                'categories:seo': ['error', { minScore: 1 }],
             },
         },
     },
