81f97dc34edd1a0db2239aacfd56f40aeb968614
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-10-27 14:53:05 +0300

    add html website functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-// Comment to CI testing
-
 import attack from "./attack";
 
-console.log(attack("Smelly Ogre", 29));
+// console.log(attack("Smelly Ogre", 29));
+
+const resultTag = document.querySelector('#resultTag')
+const creature = document.querySelector('#creature')
+const damage = document.querySelector('#damage')
+const resultButton = document.querySelector('#resultButton')
+
+resultButton.addEventListener('click', () => {
+    const result = attack(creature.value, damage.value);
+
+    resultTag.innerHTML = result
+
+})
+
